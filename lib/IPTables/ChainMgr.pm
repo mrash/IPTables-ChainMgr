ba1b6ff4587ea3c7746da55f9f056ac5b55ca631
Michael Rash <mbr@cipherdyne.org>
2012-03-02 22:29:10 -0500

    bumped version to 1.2

--- lib/IPTables/ChainMgr.pm
+++ lib/IPTables/ChainMgr.pm
@@ -10,7 +10,7 @@
 #
 # Author: Michael Rash (mbr@cipherdyne.org)
 #
-# Version: 1.1
+# Version: 1.2
 #
 ##############################################################################
 #
@@ -26,7 +26,7 @@ use strict;
 use warnings;
 use vars qw($VERSION);
 
-$VERSION = '1.1';
+$VERSION = '1.2';
 
 sub new() {
     my $class = shift;
