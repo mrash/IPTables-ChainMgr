c2b5a0af2e75a25364e869335bf5d8427238c20a
Michael Rash <mbr@cipherdyne.org>
2016-12-03 16:06:42 -0500

    1.6 release

--- lib/IPTables/ChainMgr.pm
+++ lib/IPTables/ChainMgr.pm
@@ -10,7 +10,7 @@
 #
 # Author: Michael Rash (mbr@cipherdyne.org)
 #
-# Version: 1.5
+# Version: 1.6
 #
 ##############################################################################
 #
@@ -26,7 +26,7 @@ use strict;
 use warnings;
 use vars qw($VERSION);
 
-$VERSION = '1.5';
+$VERSION = '1.6';
 
 sub new() {
     my $class = shift;
